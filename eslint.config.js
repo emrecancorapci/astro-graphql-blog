cb2dd75e5961e4c4154273d3f44db45efaaef222
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-12-29 16:28:33 +0300

    Update ESLint configuration, refactoring codebase to update html tag classes

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,6 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
 import eslintPluginTailwindCSS from "eslint-plugin-tailwindcss";
-import eslintParserTypescript from '@typescript-eslint/parser';
-import eslintParserAstro from 'astro-eslint-parser';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -13,27 +11,20 @@ export default [
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommendedTypeChecked,
+	...eslintPluginAstro.configs.recommended,
 	...eslintPluginTailwindCSS.configs["flat/recommended"],
 	{
 		languageOptions: {
-			parser: eslintParserTypescript,
 			parserOptions: {
 				project: true,
-				
 				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 				tsconfigRootDir: import.meta.dirname,
-				extraFileExtensions: [".astro"],
 			},
-			overrides: [
-				{
-					files: ["*.astro"],
-					parser: eslintParserAstro,
-					// Parse the script in `.astro` as TypeScript by adding the following configuration.
-					parserOptions: {
-						parser: eslintParserTypescript,
-					},
-				},],
 		},
 	},
-	...eslintPluginAstro.configs.recommended,
+	{
+		rules: {
+			"@typescript-eslint/no-unsafe-return": "off",
+		}
+	}
 ];
