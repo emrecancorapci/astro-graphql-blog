080dc16ecbde9dfad63d058ca7bcfd0051ebba17
Emre Can Çorapçı <emrecancorapci@gmail.com>
2025-01-03 21:17:40 +0300

    Update configuration files for improved TypeScript checks and ESLint integration

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,44 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import globals from "globals";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
 import eslintPluginTailwindCSS from "eslint-plugin-tailwindcss";
+import eslintParserAstro from "astro-eslint-parser";
 
 export default tseslint.config(
-	{
-		ignores: ['**/dist', '**/node_modules', '**/.astro', '**/.github', '**/.changeset'],
-	},
+  {
+    ignores: [
+      "**/dist",
+      "**/node_modules",
+      "**/.astro",
+      "**/.github",
+      "**/.changeset",
+    ],
+  },
 
-	// JavaScript
-	eslint.configs.recommended,
-	// TypeScript Type Checked
-	...tseslint.configs.recommendedTypeChecked,
-	{
+  // Base
+  eslint.configs.recommended, 
+	tseslint.configs.recommended,
+	eslintPluginTailwindCSS.configs["flat/recommended"],
+
+	// TypeScript
+  {
+		files: ["*.ts"],
     languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+		extends: [tseslint.configs.strictTypeChecked]
+  },
+
+  // Astro
+	eslintPluginAstro.configs.recommended,
+  {
+    files: ["*.astro"],
+    languageOptions: {
+			parser: eslintParserAstro,
       parserOptions: {
         project: true,
         tsconfigRootDir: import.meta.dirname,
@@ -23,18 +46,11 @@ export default tseslint.config(
     },
   },
 
-	// Set globals for Node scripts.
-	{
-		files: ['scripts/**'],
-		languageOptions: {
-			globals: globals.node,
-		},
-	},
-
-	// Tailwind CSS
-	...eslintPluginTailwindCSS.configs["flat/recommended"],
-
-
-	// Astro
-	...eslintPluginAstro.configs.recommended,
+  // Set globals for Node scripts.
+  {
+    files: ["scripts/**"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
 );
